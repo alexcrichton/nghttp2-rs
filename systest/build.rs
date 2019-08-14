30e53b3846002d3563d5a247eae07e89cbc6ea8b
Alex Crichton <alex@alexcrichton.com>
2019-08-14 07:17:54 -0700

    Handle `ssize_t` on MSVC in systest

--- systest/build.rs
+++ systest/build.rs
@@ -4,6 +4,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let target = env::var("TARGET").unwrap();
     let root = PathBuf::from(env::var_os("DEP_NGHTTP2_ROOT").unwrap());
     let mut cfg = ctest::TestGenerator::new();
     cfg.header("nghttp2/nghttp2.h")
@@ -28,6 +29,15 @@ fn main() {
                 field.to_string()
             }
         });
+
+    // Apparently MSVC doesn't have `ssize_t` defined as a type
+    if target.contains("msvc") {
+        match env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap().as_str() {
+            "64" => { cfg.define("ssize_t", Some("int64_t")); }
+            "32" => { cfg.define("ssize_t", Some("int32_t")); }
+            s => panic!("unknown pointer size: {}", s),
+        }
+    }
 
     cfg.generate("../src/lib.rs", "all.rs");
 }
