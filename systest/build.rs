872f89281f4219dff09a461deaca53af978595a4
Chris Barrick <cbarrick1@gmail.com>
2026-02-27 17:41:13 -0500

    Update to the latest version of `ctest`

--- systest/build.rs
+++ systest/build.rs
@@ -1,4 +1,7 @@
+extern crate cc;
 extern crate ctest;
+
+use ctest::{Field, Struct};
 
 use std::env;
 use std::path::PathBuf;
@@ -6,46 +9,55 @@ use std::path::PathBuf;
 fn main() {
     let target = env::var("TARGET").unwrap();
     let root = PathBuf::from(env::var_os("DEP_NGHTTP2_ROOT").unwrap());
+    let include = root.join("include");
+
     let mut cfg = ctest::TestGenerator::new();
 
-    // Apparently MSVC doesn't have `ssize_t` defined as a type
+    cfg.include(&include)
+        .header("nghttp2/nghttp2.h")
+        .header("nghttp2/nghttp2ver.h");
+
+    // MSVC doesn't have `ssize_t` defined as a type.
     if target.contains("msvc") {
         match env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap().as_str() {
-            "64" => {
-                cfg.define("ssize_t", Some("int64_t"));
-            }
-            "32" => {
-                cfg.define("ssize_t", Some("int32_t"));
-            }
+            "64" => cfg.define("ssize_t", Some("int64_t")),
+            "32" => cfg.define("ssize_t", Some("int32_t")),
             s => panic!("unknown pointer size: {}", s),
-        }
+        };
     }
 
-    cfg.header("nghttp2/nghttp2.h")
-        .include(root.join("include"))
-        .type_name(|n, _is_struct, _is_union| n.to_string())
-        .skip_struct(|name| {
-            // TODO: dox
-            name == "nghttp2_session"
-                || name == "nghttp2_rcbuf"
-                || name == "nghttp2_session_callbacks"
-                || name == "nghttp2_option"
-                || name == "nghttp2_hd_deflater"
-                || name == "nghttp2_hd_inflater"
-                || name == "nghttp2_stream"
-        })
-        .field_name(|_struct, field| {
-            if field == "type_" {
-                "type".to_string()
-            } else {
-                field.to_string()
-            }
-        })
-        .skip_signededness(move |_ty| {
-            // skip signededness checks on MSVC since lots of enums switch, and
-            // it doesn't really matter that much anyway
-            target.contains("msvc")
-        });
+    // Some nghttp2 structs have a field named `type`.
+    // This is a reserved keyword in Rust, which bindgen renames to `type_`.
+    cfg.rename_struct_field(|_st: &Struct, field: &Field| match field.ident() {
+        "type_" => Some("type".to_string()),
+        _ => None,
+    });
 
-    cfg.generate("../src/lib.rs", "all.rs");
+    // Skip struct tests for opaque types.
+    cfg.skip_struct(|st: &Struct| match st.ident() {
+        "nghttp2_session" => true,
+        "nghttp2_rcbuf" => true,
+        "nghttp2_session_callbacks" => true,
+        "nghttp2_option" => true,
+        "nghttp2_hd_deflater" => true,
+        "nghttp2_hd_inflater" => true,
+        "nghttp2_stream" => true,
+        _ => false,
+    });
+
+    // Skip signededness tests on MSVC since lots of enums switch,
+    // and it doesn't really matter that much anyway.
+    cfg.skip_signededness(move |_ty| target.contains("msvc"));
+
+    // Generate the test files.
+    let mut path = cfg.generate_files("../src/lib.rs", "ctest.rs").unwrap();
+
+    // Path points to `ctest.rs`. We want `ctest.c`.
+    path.set_file_name("ctest.c");
+
+    // Build the generated C code.
+    cc::Build::new()
+        .include(&include)
+        .file(&path)
+        .compile("ctest");
 }
